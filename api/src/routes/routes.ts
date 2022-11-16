9d2a48a8311214b5640e3e6a344a64b34cda9534
darkeny <darkenpaulo00@gmail.com>
2022-11-16 15:27:02 +0200

    add prisma

--- api/src/routes/routes.ts
+++ api/src/routes/routes.ts
@@ -5,9 +5,7 @@ import { configMulter } from '../config/multer';
 export const routes = Router()
 
 routes.post('/posts', upload(configMulter).array('file'), (req, res) => {
-    console.log(req.files);
+   console.log(req.files);
+   
     return res.status(200).json({hello: 'succeed'})
-    
-    
-    
 })
